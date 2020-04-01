f6fa76aef9f3b340e88339a99f1f04c195030571
Mateusz Niełacny <mateusz.nielacny@gmail.com>
2020-04-01 10:51:07 +0200

    Copy cw3  for cw4

---
+++ WebApplication1/WebApplication1/DAL/MockDbService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.DAL
+{
+    public class MockDbService : IdbService
+    {
+        private static List<Student> students = new List<Student>();
+
+        public MockDbService()
+        {
+            students.Add(new Student
+            {
+                IntStudent = 1,
+                FirstName = "Jarosław",
+                LastName = "Rodak"
+            });
+            students.Add(new Student
+            {
+                IntStudent = 2,
+                FirstName = "Maciej",
+                LastName = "Polak"
+            });
+        }
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return students;
+        }
+
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+
+    }
+}
